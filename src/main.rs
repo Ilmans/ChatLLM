379464d2831c0809f65d71d8f9a4413e3b94d4f2
zuramai <saugi.dev@gmail.com>
2023-08-11 14:31:29 +0700

    feat: add arguments

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,52 @@
 use std::sync::Arc;
 
-use config::{load, Config};
+use clap::{ Command, Parser, Arg};
+use config::{ Config};
 use eyre;
 use color_eyre;
 use router::RouterState;
 use services::{user::UserServiceImpl, auth::AuthServiceImpl};
-use sqlx::{Postgres, Pool, postgres::PgPoolOptions};
+use sqlx::{  postgres::PgPoolOptions};
+
+#[derive(Debug, Parser)]
+#[clap(about="Rust LLM Server")]
+struct Arguments {
+    // The command to execute. Possible values: 'migrate' and 'start'
+    command: String
+}
+
 
 #[tokio::main]
 async fn main()  -> eyre::Result<()>{
     color_eyre::install()?;
+    let args = Command::new("command")
+        .arg(Arg::new("command").value_parser(["migrate", "start"]).default_value("start"))
+        .get_matches();
+    let command = args.get_one::<String>("command").unwrap();
+    println!("Arguments: {:?}", command);
+
+    // Load the config
+    let config = config::load()?;
+
+    // Connect to database
+    let db = PgPoolOptions::new()
+        .max_connections(30)
+        .connect(&config.database.url.to_string())
+        .await?;
+
+    if command == "migrate" {
+        sqlx::migrate!().run(&db).await?;
+        return Ok(());
+    }
+
     tracing_subscriber::fmt::init();
-    run(config::load()?).await?;
+    run(config).await?;
     Ok(())
 }
 
 async fn run(config: Config) -> eyre::Result<()> {
     tracing::info!("Connecting to database..");
 
-    // Connect to database
-    let pool = PgPoolOptions::new()
-        .max_connections(30)
-        .connect(&config.database.url.to_string())
-        .await?;
     tracing::info!("Database connected.");
 
     let state = RouterState {
