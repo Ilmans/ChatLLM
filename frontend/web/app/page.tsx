aed3311d8ec1e620c4ee9fa5073cb6934c0d208d
zuramai <saugi.dev@gmail.com>
2023-09-11 08:39:43 +0700

    feat: add fork of web-llm

---
+++ frontend/web/app/page.tsx
@@ -0,0 +1,76 @@
+"use client"
+
+import Image from 'next/image'
+import Link from 'next/link'
+import { useEffect } from 'react';
+import { ChatModule, InitProgressReport } from "@mlc-ai/web-llm";
+
+export default function Home() {
+
+  function setLabel(id: string, text: string) {
+    const label = document.getElementById(id);
+    if (label == null) {
+      throw Error("Cannot find label " + id);
+    }
+    label.innerText = text;
+  }
+
+  async function main() {
+    const chat = new ChatModule();
+
+    chat.setInitProgressCallback((report: InitProgressReport) => {
+      setLabel("init-label", report.text);
+    });
+    console.log('loading model')
+    await chat.reload("Nous-Hermes-Llama2-13b", {
+      conv_template: 'llama-2',
+    }, {
+      model_list: [
+        {
+          local_id: "Nous-Hermes-Llama2-13b",
+          model_url: "http://localhost:3000/Nous-Hermes-Llama2-13b-q4f16_1/params/",
+          required_features: ['shader-f16']
+        }
+      ],
+      model_lib_map: {
+        'Nous-Hermes-Llama2-13b-q4f16_1': 'http://localhost:3000/Nous-Hermes-Llama2-13b-q4f16_1/Nous-Hermes-Llama2-13b-q4f16_1-webgpu.wasm',
+      }
+    })
+    console.log('loaded')
+
+    const generateProgressCallback = (_step: number, message: string) => {
+      setLabel("generate-label", message);
+    };
+
+    const prompt0 = "What is the capital of Canada?";
+    setLabel("prompt-label", prompt0);
+    const reply0 = await chat.generate(prompt0, generateProgressCallback);
+    console.log(reply0);
+
+    const prompt1 = "Can you write a poem about it?";
+    setLabel("prompt-label", prompt1);
+    const reply1 = await chat.generate(prompt1, generateProgressCallback);
+    console.log(reply1);
+
+    console.log(await chat.runtimeStatsText());
+  }
+
+  useEffect(() => {main()}, []);
+  return (
+    <main>
+      <div className="container mx-auto text-4xl">
+        <h1>Chat App</h1>
+        <h2>WebLLM Test Page</h2>
+        Open console to see output
+        <label id="init-label"> </label>
+
+        <h3>Prompt</h3>
+        <label id="prompt-label"> </label>
+
+        <h3>Response</h3>
+        <label id="generate-label"> </label>
+        <label id="stats-label"> </label>
+      </div>
+    </main>
+  )
+}
