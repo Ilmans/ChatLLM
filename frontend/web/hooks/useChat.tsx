a8e6138b10abace8446aa3a562f5f1d4d2aff751
zuramai <saugi.dev@gmail.com>
2023-09-12 20:40:51 +0700

    feat: add loading model animation

--- frontend/web/hooks/useChat.tsx
+++ frontend/web/hooks/useChat.tsx
@@ -1,47 +1,46 @@
 import { createContext, useContext, useState } from "react"
-import { ChatModule, InitProgressReport } from "@mlc-ai/web-llm";
+import { ChatModule, InitProgressCallback, InitProgressReport, ModelRecord } from "@mlc-ai/web-llm";
 
 interface ChatContextType {
   chat: ChatModule | null 
-  loadChat: () => Promise<void>
+  loadChat: (model_id: string, onModelLoadingCb?: InitProgressCallback) => Promise<void>
+  availableModels: ModelRecord[]
 }
 
 const ChatContext = createContext<ChatContextType>({} as ChatContextType)
 
 export function ChatProvider({children}: {children: any}) {
     const [chat, setChat] = useState<ChatModule|null>(null)
+    const availableModels = [
+      {
+        local_id: "Nous-Hermes-Llama2-13b",
+        model_url: "http://localhost:3001/models/Nous-Hermes-Llama2-13b-q4f16_1/params/",
+        required_features: ['shader-f16']
+      }
+    ]
 
-    const loadChat = async () => {
-       console.log('load chat')
-        const newChat = new ChatModule()
-
-        newChat.setInitProgressCallback((report: InitProgressReport) => {
-          console.log(report.text)
-        });
-
-        await newChat.reload("Nous-Hermes-Llama2-13b", {
-              conv_template: 'llama-2',
-            }, {
-              model_list: [
-                {
-                  local_id: "Nous-Hermes-Llama2-13b",
-                  model_url: "http://localhost:3001/models/Nous-Hermes-Llama2-13b-q4f16_1/params/",
-                  required_features: ['shader-f16']
-                }
-              ],
-              model_lib_map: {
-                'Nous-Hermes-Llama2-13b-q4f16_1': 'http://localhost:3001/models/Nous-Hermes-Llama2-13b-q4f16_1/Nous-Hermes-Llama2-13b-q4f16_1-webgpu.wasm',
-              }
-            })
-            console.log('loaded')
-        
-            const generateProgressCallback = (_step: number, message: string) => {
-                console.log('generate label', message)
-            };
+    const loadChat = async (model_id: string, onModelLoadingCb?: (report: InitProgressReport) => void) => {
+      const newChat = new ChatModule()
+      
+      if(onModelLoadingCb)
+        newChat.setInitProgressCallback(onModelLoadingCb);
+    
+      console.log('load chat', onModelLoadingCb)
+      await newChat.reload(model_id, { conv_template: 'llama-2' }, {
+        model_list: availableModels,
+        model_lib_map: {
+          'Nous-Hermes-Llama2-13b-q4f16_1': 'http://localhost:3001/models/Nous-Hermes-Llama2-13b-q4f16_1/Nous-Hermes-Llama2-13b-q4f16_1-webgpu.wasm',
+        }
+      })
+      console.log('loaded')
+  
+      const generateProgressCallback = (_step: number, message: string) => {
+          console.log('generate label', message)
+      };
     }
 
     return (
-        <ChatContext.Provider value={{chat, loadChat}}>
+        <ChatContext.Provider value={{chat, loadChat, availableModels}}>
             {children}
         </ChatContext.Provider>
     )
--- frontend/web/src/app/(dashboard)/dashboard/page.tsx
+++ frontend/web/src/app/(dashboard)/dashboard/page.tsx
@@ -2,16 +2,24 @@
 
 import ChatUI from "@/components/ChatUI";
 import Page from "@/components/layouts/Page";
-import { Card, LoadingOverlay, Text, Title } from "@mantine/core";
+import { Card, Group, LoadingOverlay, Select, Text, Title } from "@mantine/core";
 import { useEffect, useState } from "react";
 import { useChatContext } from "../../../../hooks/useChat";
 import LoadingOverlayText from "@/components/LoadingOverlayText";
+import { InitProgressCallback, InitProgressReport } from "@mlc-ai/web-llm";
 
 export default function Dashboard() {
     const chat = useChatContext()
     const [chatLoaded, setChatLoaded] = useState(false)
+    const [loadingText, setLoadingText] = useState('')
+
     useEffect(() => {
-        chat.loadChat()
+        const modelLoadingCallback = (report: InitProgressReport) => {
+            console.log('model loading..')
+            setLoadingText(`Loading model.. (${Math.floor(report.progress * 100)}%)`)
+        }
+
+        chat.loadChat('Nous-Hermes-Llama2-13b', modelLoadingCallback)
             .then(() => {
                 setChatLoaded(true)
             })
@@ -24,13 +32,19 @@ export default function Dashboard() {
             >
                 <Card withBorder radius="sm">
                     <Card.Section withBorder inheritPadding py={"xs"}>
-                        <Title order={4} weight={600}>Chat</Title>
+                        <Group position="apart">
+                            <Title order={4} weight={600}>Chat</Title>
+                            <Select defaultValue={'Nous-Hermes-Llama2-13b'} data={chat.availableModels.map(m => ({
+                                value: m.local_id,
+                                label: m.local_id
+                            }))}></Select>
+                        </Group>
                     </Card.Section>
                     <Card.Section inheritPadding py={"xs"} pos={"relative"}>
                         <LoadingOverlay 
                             visible={!chatLoaded} 
                             loader={
-                                <LoadingOverlayText text="Loading.." visible={!chatLoaded}/>
+                                <LoadingOverlayText text={loadingText} visible={!chatLoaded}/>
                             }
                         />
                         <ChatUI></ChatUI>
