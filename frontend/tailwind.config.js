a3a508bfda7cc25b8c6398218858f01fcbbf3634
zuramai <saugi.dev@gmail.com>
2024-01-08 13:34:01 +0700

    fix: tailwind whitelist

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,10 +5,7 @@ module.exports = {
   darkMode: ["class"],
   
   content: [
-    './pages/**/*.{ts,tsx,vue}',
-    './components/**/*.{ts,tsx,vue}',
-    './app/**/*.{ts,tsx,vue}',
-    './apps/frontend/src/**/*.{ts,tsx,vue}',
+    './src/**/*.{ts,tsx,vue}',
 	],
   
   theme: {
