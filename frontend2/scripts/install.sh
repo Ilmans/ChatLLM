987fbf1b3b12d69edd37b34114d87d45bb87af4d
zuramai <saugi.dev@gmail.com>
2024-05-28 10:36:02 +0700

    chore: remove frontend2 frontend3

--- frontend2/scripts/install.sh
+++
@@ -1,6 +0,0 @@
-cd packages/web-llm
-
-# Install Apache TVM
-git clone https://github.com/apache/tvm tvm_home
-cd tvm/web
-pnpm install
--- frontend2/web/src/components/Logo.tsx
+++
@@ -1,9 +0,0 @@
-import { Box, Title } from "@mantine/core";
-
-export default function Logo() {
-    return (
-        <Title sx={(theme) => ({
-            fontSize: theme.fontSizes.lg
-        })}>RustLLM</Title>
-    )
-}
