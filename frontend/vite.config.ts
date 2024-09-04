6ab70a44a71d9f7d78434743b2787f2733abdda8
zuramai <saugi.dev@gmail.com>
2024-09-05 05:39:31 +0700

    fix: update workbox setting

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,27 +1,26 @@
 /// <reference types='vitest' />
-import path from 'path'
-import {fileURLToPath} from 'url'
+import path from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import topLevelAwait from "vite-plugin-top-level-await";
-import {VitePWA} from 'vite-plugin-pwa'
+import topLevelAwait from 'vite-plugin-top-level-await';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   publicDir: 'static',
   cacheDir: '../../node_modules/.vite/frontend',
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   build: {
-    target: 'es2020'
-  },  
+    target: 'es2020',
+  },
   server: {
     port: 5174,
     host: 'localhost',
   },
-  
 
   preview: {
     port: 4300,
@@ -29,31 +28,35 @@ export default defineConfig({
   },
 
   plugins: [
-    vue(), 
-    VitePWA({ 
+    vue(),
+    VitePWA({
+      workbox: {
+        globPatterns: ['**/*'],
+        maximumFileSizeToCacheInBytes: 6000000
+      },
       registerType: 'autoUpdate',
       devOptions: {
-        enabled: true
+        enabled: true,
       },
       manifest: {
-        name: "ChatLLM",
-        description: "LLM Chat Application",
-        short_name: "LLM",
+        name: 'ChatLLM',
+        description: 'LLM Chat Application',
+        short_name: 'LLM',
         icons: [
           {
-            src: "logo.png",
+            src: 'logo.png',
             sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
+            type: 'image/png',
+          },
+        ],
+      },
     }),
     topLevelAwait({
       // The export name of top-level await promise for each chunk module
-      promiseExportName: "__tla",
+      promiseExportName: '__tla',
       // The function to generate import names of top-level await promise in each chunk module
-      promiseImportName: i => `__tla_${i}`
-    })
+      promiseImportName: (i) => `__tla_${i}`,
+    }),
   ],
 
   // Uncomment this if you are using workers.
